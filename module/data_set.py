e452c9ed575c38bfe52cc7b443856b610a02e532
Dongjin-1203 <hambur1203@gmail.com>
2025-05-30 17:36:39 +0900

    시각화 조건부 시행, 변수 선언 통일화

--- module/data_set.py
+++ module/data_set.py
@@ -2,7 +2,7 @@ from torch.utils.data import DataLoader
 import torch
 import matplotlib.pyplot as plt
 
-def data_split(data, class_names):
+def data_split(data, class_names, visualize=False):
     # 훈련/검증 분리
     train_size = int(0.8 * len(data))
     val_size = len(data) - train_size
@@ -15,14 +15,15 @@ def data_split(data, class_names):
     train_iter = iter(train_loader)
     images, labels = next(train_iter)
 
-    plt.figure(figsize=(15, 5))
-    for idx in range(10):
-        ax = plt.subplot(2, 5, idx+1)
-        img = images[idx].numpy().transpose((1, 2, 0))
-        plt.imshow(img)
-        plt.title(class_names[labels[idx]])
-        plt.axis('off')
-    plt.suptitle("Sample Training Images")
-    plt.show()
+    if visualize:
+        plt.figure(figsize=(15, 5))
+        for idx in range(10):
+            ax = plt.subplot(2, 5, idx+1)
+            img = images[idx].numpy().transpose((1, 2, 0))
+            plt.imshow(img)
+            plt.title(class_names[labels[idx]])
+            plt.axis('off')
+        plt.suptitle("Sample Training Images")
+        plt.show()
 
     return train_loader, val_loader
